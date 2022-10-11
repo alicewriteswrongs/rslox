68594346f7b5312ee112bc57aa298938d53be348
Alice Pote <alice.writes.wrongs@gmail.com>
2022-10-11 15:34:09 -0400

    add constants

--- src/chunk.rs
+++ src/chunk.rs
@@ -1,7 +1,11 @@
 use std::fmt;
 
+use crate::value::Value;
+
 #[derive(Debug)]
 pub enum OpCode {
+    // the usize here is the index
+    OpConstant(usize),
     OpReturn,
 }
 
@@ -9,6 +13,7 @@ impl fmt::Display for OpCode {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             OpCode::OpReturn => f.pad("OP_RETURN"),
+            OpCode::OpConstant(value) => f.pad(&format!("OP_CONSTANT: {}", value)),
         }
     }
 }
@@ -16,22 +21,43 @@ impl fmt::Display for OpCode {
 #[derive(Debug)]
 pub struct Chunk {
     code: Vec<OpCode>,
+    constants: Vec<Value>,
 }
 
 impl Chunk {
     pub fn init() -> Chunk {
-        Chunk { code: vec![] }
+        Chunk {
+            code: vec![],
+            constants: vec![],
+        }
     }
 
     pub fn write(&mut self, byte: OpCode) {
         self.code.push(byte);
     }
 
+    pub fn add_constant(&mut self, value: Value) -> usize {
+        self.constants.push(value);
+        self.constants.len() - 1
+    }
+
     pub fn disassemble(&self, name: &str) {
         println!("== {} ==", name);
 
         for (i, opcode) in self.code.iter().enumerate() {
-            println!("{:0>4} {}", i, opcode);
+            match opcode {
+                OpCode::OpConstant(index) => {
+                    println!(
+                        "{:0>4} {} '{}'",
+                        i,
+                        OpCode::OpConstant(*index),
+                        self.constants[*index]
+                    );
+                }
+                OpCode::OpReturn => {
+                    println!("{:0>4} {}", i, OpCode::OpReturn);
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 pub mod chunk;
+pub mod value;
 
 use chunk::{Chunk, OpCode};
 
 fn main() {
     let mut chunk = Chunk::init();
+    let index = chunk.add_constant(1.2);
+    chunk.write(OpCode::OpConstant(index));
     chunk.write(OpCode::OpReturn);
     chunk.disassemble("test chunk");
 }
